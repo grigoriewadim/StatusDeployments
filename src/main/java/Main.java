b48216b03696b133ba8d22b98849a5b6c934a05b
grigoriewadim <grigoriewadim@yandex.ru>
2019-06-28 10:11:57 +0300

    I changed logic, in the separate file there is a config now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,3 @@
-import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.IndexedColors;
 import org.jboss.as.controller.client.ModelControllerClient;
 import org.jboss.as.controller.client.helpers.ClientConstants;
 import org.jboss.as.controller.client.helpers.Operations;
@@ -17,32 +13,81 @@ import javax.swing.border.TitledBorder;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import javax.swing.text.BadLocationException;
 import java.awt.*;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
-    private static String[] servers = {"127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1"}; //List of servers
     private static int index = 0;
     private static ModelControllerClient client;
     private static JPanel appContainer = new JPanel(new GridLayout());
     private static JFrame frame = new JFrame("WildFly Deployment's Status");
+    private static JFrame finderFrame = new JFrame("Find Modules");
+    private static JFrame warningFrame = new JFrame("Предупреждение!");
     private static int xDimmension = 1300;
     private static int yDimmension = 775;
     private static JPanel allServers = new JPanel(new GridLayout());
-    private static final ArrayList<String> exportList = new ArrayList<>(); //ip and app
+    private static ArrayList<String> hostList = new ArrayList<>();
+    private static int counter;
+
+    static class ReadConfig { //Читаем конфиг
+        static String login;
+        static String password;
+
+        ReadConfig() throws Exception {
+            try {
+                String filename = "config.cfg";
+                BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (!Character.isLetter(line.charAt(0))) {
+                        hostList.add(line); //Добавляем в список
+                    } else {
+                        if (line.contains("login")) {
+                            login = line.substring(line.indexOf(":") + 1);
+                        } else if (line.contains("password")) {
+                            password = line.substring(line.indexOf(":") + 1);
+                        } else {
+                            MessageBox();
+                        }
+                    }
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                MessageBox(new Exception("Не найден файл!"));
+                System.exit(0);
+            } catch (NullPointerException e) {
+                MessageBox(new Exception(e));
+                System.exit(0);
+            }
+        }
+    }
+
+    static class WarningFrameReload {
+        WarningFrameReload(String warningHost) {
+            Font font = new Font("Arial Black", Font.BOLD, 11);
+            warningFrame.setSize(500, 90);
+            JTextArea warningField = new JTextArea();
+            warningField.setFont(font);
+            warningField.setEnabled(false);
+            warningField.setText("Внимание! На хосте " + warningHost + " запущено 5 и более модулей. " + "\n" +
+                    "Это может повлиять на работоспособность сервера.");
+            warningFrame.add(warningField);
+            warningFrame.setLocationByPlatform(true);
+            warningFrame.setVisible(true);
+        }
+    }
 
     private static void MessageBox(Exception exception) {
         JOptionPane.showMessageDialog(new JFrame(), exception, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
-    private static void MessageBox(String information) {
-        JOptionPane.showMessageDialog(new JFrame(), information, "Successfully", JOptionPane.INFORMATION_MESSAGE);
+    private static void MessageBox() {
+        JOptionPane.showMessageDialog(new JFrame(), "Не корректный config", "Successfully", JOptionPane.INFORMATION_MESSAGE);
     }
 
     private static String Splitter(String string) {
@@ -55,116 +100,34 @@ public class Main {
             list.clear();
             appContainer.removeAll();
             serverPanel.removeAll();
+            hostList.clear();
             index = 0;
+            warningFrame.dispose();
+            finderFrame.dispose();
             frame.dispose();
             System.gc();
             new Runner();
-        }
-    }
 
-    static String GetDate() {
-        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyy_HH-mm-ss");
-        Date data = new Date();
-        return dateFormat.format(data);
-    }
-
-    static HSSFCellStyle enabledStyling(HSSFWorkbook workbook) {
-        final HSSFCellStyle style = workbook.createCellStyle();
-        style.setBorderBottom(BorderStyle.DOTTED);
-        HSSFFont font = workbook.createFont();
-        font.setColor(IndexedColors.GREEN.getIndex());
-        style.setFont(font);
-        return style;
-    }
-
-    static HSSFCellStyle disabledStyling(HSSFWorkbook workbook) {
-        final HSSFCellStyle style = workbook.createCellStyle();
-        style.setBorderBottom(BorderStyle.DOTTED);
-        HSSFFont font = workbook.createFont();
-        font.setColor(IndexedColors.RED.getIndex());
-        style.setFont(font);
-        return style;
-    }
-
-    static HSSFCellStyle serverStyle(HSSFWorkbook workbook) {
-        final HSSFCellStyle style = workbook.createCellStyle();
-        style.setBorderBottom(BorderStyle.DOTTED);
-        style.setAlignment(HorizontalAlignment.CENTER);
-        HSSFFont font = workbook.createFont();
-        font.setFontHeightInPoints((short) 12);
-        font.setBold(true);
-        style.setFont(font);
-        return style;
-    }
-
-    private static class ExportStatus {
-        ExportStatus() throws IOException {
-            HSSFWorkbook workbook = new HSSFWorkbook();
-            HSSFSheet sheet = workbook.createSheet("Deployment's Status");
-            HSSFCellStyle unlockedStyle = workbook.createCellStyle();
-            unlockedStyle.setLocked(true);
-            sheet.protectSheet("password");
-            int countSrvRow = 0;
-            for (int i = 0; i < exportList.size(); i++) {
-                sheet.setColumnWidth(i, 9000);
-            }
-            for (String server : servers) {
-                countSrvRow++;
-                sheet.setDefaultRowHeight((short) 300);
-                HSSFRow headerRow = sheet.createRow(countSrvRow);
-                HSSFCell headerCell;
-                headerCell = headerRow.createCell(0);
-                headerCell.setCellStyle(serverStyle(workbook));
-                headerCell.setCellValue(server);
-                int columns = 1;
-                for (String anExportList : exportList) {
-                    StringTokenizer tokenizer = new StringTokenizer(anExportList, "<=>");
-                    String ip = tokenizer.nextToken();
-                    String app = tokenizer.nextToken();
-                    String status = tokenizer.nextToken();
-                    if (ip.equals(headerCell.toString())) {
-                        if (status.contains("true")) {
-                            HSSFCell appCell;
-                            appCell = headerRow.createCell(columns);
-                            appCell.setCellStyle(enabledStyling(workbook));
-                            appCell.setCellValue(app);
-                            columns++;
-                        }
-                        if (status.contains("false")) {
-                            HSSFCell appCell;
-                            appCell = headerRow.createCell(columns);
-                            appCell.setCellStyle(disabledStyling(workbook));
-                            appCell.setCellValue(app);
-                            columns++;
-                        }
-                    }
-                }
-            }
-            sheet.createFreezePane(1, 1);
-
-            File exportFile = new File("exportstatus_" + GetDate() + ".xls");
-            FileOutputStream outFile = new FileOutputStream(exportFile);
-            workbook.write(outFile);
-            MessageBox("Created File: " + exportFile.getAbsolutePath());
-            outFile.close();
         }
     }
 
     private static class StatusDeployments extends JFrame {
         static HashMap<String, String> applicationMap = new HashMap<>();
         static ArrayList<JPanel> panelsList = new ArrayList<>();
+        String[] transportModules = {"custodian", "dpl", "logger", "seap", "transport"};
 
         StatusDeployments() throws IOException, BadLocationException {
-            final String username = "admin";
-            final String password = "admin";
+            final String username = ReadConfig.login;
+            final String password = ReadConfig.password;
             applicationMap.clear();
             panelsList.clear();
             appContainer.setSize(xDimmension - 6, yDimmension - 60);
             appContainer.setBorder(BorderFactory.createLineBorder(Color.black));
             int localport = 9990;
+            counter = 0;
             try {
                 client = ModelControllerClient.Factory.create(
-                        InetAddress.getByName(servers[index]), localport,
+                        InetAddress.getByName(String.valueOf(hostList.get(index))), localport,
                         callbacks -> {
 
                             for (Callback current : callbacks) {
@@ -183,7 +146,7 @@ public class Main {
                             }
                         });
             } catch (java.net.UnknownHostException e) {
-                MessageBox(new Exception("Error of connection with the server " + servers[index] + " " + e));
+                MessageBox(new Exception("Error of connection with the server " + hostList.get(index) + " " + e));
             } catch (java.lang.NullPointerException e) {
                 MessageBox(new Exception("Sorry! Error... " + e));
             }
@@ -198,7 +161,10 @@ public class Main {
                             Arrays.toString(namesList.get("enabled").toString().split(",")));
                 }
                 JPanel serverPanel = new JPanel(new FlowLayout());
-                serverPanel.setBorder(new TitledBorder(new EtchedBorder(), "Server: " + servers[index]));
+                serverPanel.setBorder(new TitledBorder(new EtchedBorder(), "Server: " + hostList.get(index)));
+                for (String transportModule : transportModules) {
+                    applicationMap.entrySet().removeIf(entry -> entry.getKey().contains(transportModule));
+                }
                 for (Map.Entry<String, String> entry : applicationMap.entrySet()) {
                     String key = entry.getKey();
                     String value = entry.getValue();
@@ -211,6 +177,7 @@ public class Main {
                         textArea.append(Splitter(key) + "\n");
                         enabledPanel.add(textArea);
                         panelsList.add(enabledPanel);
+                        counter++;
                     } else if (value.contains("false")) {
                         JPanel disabledPanel = new JPanel(new GridLayout());
                         disabledPanel.setPreferredSize(new Dimension(250, 25));
@@ -219,13 +186,10 @@ public class Main {
                         disabledPanel.add(textArea);
                         panelsList.add(disabledPanel);
                     }
-                    exportList.add(servers[index] + "<=>" + Splitter(key) + "<=>" + Splitter(value) + "\n");
                 }
-
                 for (int i = 0; i < applicationMap.size(); i++) {
                     serverPanel.add(panelsList.get(i));
                 }
-
                 allServers.setPreferredSize(new Dimension(xDimmension - 7, panelsList.size() * 35));
                 allServers.add(serverPanel);
 
@@ -236,42 +200,65 @@ public class Main {
             } catch (IOException e) {
                 MessageBox(e);
             }
-
             index++;
             frame.add(appContainer);
             client.close();
         }
     }
 
+    static String FinderDialog() {
+        return JOptionPane.showInputDialog(null, "What's find ?", "Find Module", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    static class FindText {  //Класс для поиска запрашиваемого текста в JTextArea
+        FindText() {
+            String findText = FinderDialog();
+            if (findText.equals("")) {
+                JOptionPane.showMessageDialog(null, " Null String! ");
+            }
+            finderFrame = new JFrame("Modules");
+            finderFrame.setSize(300, 500);
+            finderFrame.setLocation(600,300);
+            JPanel finderPanel = new JPanel();
+
+
+
+
+
+            finderFrame.add(finderPanel);
+            finderFrame.setVisible(true);
+        }
+    }
+
     static class Runner {
 
         Runner() throws IOException, BadLocationException {
-
+            try {
+                new ReadConfig();
+            } catch (Exception e) {
+                MessageBox(e);
+            }
             frame.setSize(xDimmension, yDimmension);
             frame.setLocation(300, 200);
             frame.setLayout(null);
             frame.setResizable(false);
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            for (String ignored : servers) {
+            for (String counterHosts : hostList) {
                 new StatusDeployments();
+                //if (counter > 5) new WarningFrameReload(counterHosts);
             }
             JScrollPane scrollPane = new JScrollPane(allServers,
                     ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
             appContainer.add(scrollPane);
-
             JPanel buttonPanel = new JPanel(new GridLayout());
             JPanel infoPanel = new JPanel(new GridLayout());
-            buttonPanel.setBounds(0, 717, xDimmension - 900, 29);
+            buttonPanel.setBounds(0, 717, xDimmension - 1000, 29);
             infoPanel.setBounds(301, 717, xDimmension - 308, 29);
             buttonPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             infoPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             buttonPanel.setBackground(Color.lightGray);
             infoPanel.setBackground(Color.lightGray);
-            JButton reloadButton = new JButton();
-            JButton exportButton = new JButton();
-            exportButton.setText("EXPORT");
-            exportButton.setEnabled(true);//
-            reloadButton.setText("RELOAD");
+            JButton reloadButton = new JButton("RELOAD");
             reloadButton.addActionListener(e -> {
                 try {
                     new Reload(StatusDeployments.applicationMap, StatusDeployments.panelsList, appContainer, allServers);
@@ -279,15 +266,13 @@ public class Main {
                     MessageBox(new Exception("Mistake during restart of the program! " + e1));
                 }
             });
-            exportButton.addActionListener(e -> {
-                try {
-                    new ExportStatus();
-                } catch (IOException e1) {
-                    MessageBox(new Exception("Error of the export! " + e1));
-                }
+            JButton findButton = new JButton("FIND");
+            findButton.addActionListener(e -> {
+                new FindText();
             });
+
             buttonPanel.add(reloadButton);
-            buttonPanel.add(exportButton);
+            buttonPanel.add(findButton);
             frame.add(buttonPanel);
             frame.add(infoPanel);
             frame.setVisible(true);
